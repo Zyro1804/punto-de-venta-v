fde18e31b9730c629dfb384f46f5c6e9d797fae2
Jorge Heriberto Flores Reyes <jorgefloresr@tabasco.gob.mx>
2026-08-24 09:59:46 -0600

    header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { AvatarModule } from '@openng/optimus-ui/avatar';
+import { ButtonModule } from '@openng/optimus-ui/button';
 import { ToolbarModule } from '@openng/optimus-ui/toolbar';
 
 @Component({
-  imports: [ToolbarModule, AvatarModule],
+  imports: [ToolbarModule, AvatarModule, ButtonModule],
   selector: 'app-header',
   styleUrl: './header.css',
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+
+  @Output() menuClick = new EventEmitter<any>()
+
+  openMenu() {
+    this.menuClick.emit();
+  }
+}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Menu } from '../../components/menu/menu';
 import { Header } from '../../components/header/header';
+import { DrawerModule } from '@openng/optimus-ui/drawer';
 
 @Component({
-  imports: [Menu, Header],
+  imports: [Menu, Header, DrawerModule],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
-export class Home {}
+export class Home {
+
+    menuVisible = signal(false);
+
+  openMenu() {
+    this.menuVisible.set(true);
+  }
+
+  closeMenu() {
+    this.menuVisible.set(false);
+  }
+}
